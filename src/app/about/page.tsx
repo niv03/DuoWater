c053dd2a57ec5f4c43ba50940c214c2276dcb445
niv03 <nivit@ualberta.ca>
2024-03-17 01:20:07 -0600

    updated

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,8 +5,8 @@ import Breadcrumb from "@/components/Common/Breadcrumb";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Advertise Page | Free Next.js Template for Startup and SaaS",
-  description: "This is About Page for Startup Nextjs Template",
+  title: "Advertise Page",
+  description: "",
   // other metadata
 };
 
